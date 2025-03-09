1bbe3eec8f40b2079339c9239a9abf8bb47918f0
Tnvrmomo <tnvrmomo@gmail.com>
2025-03-10 00:58:02 +0600

    Your commit message

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 // filepath: /C:/Users/LAPAID/Downloads/Women Website/tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
